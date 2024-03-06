5217c7265a1bc1fa52a39f61aab18a7a4edd6cba
Gufron Aridho Triasa Putra Irawan <119670148+GufronAridho@users.noreply.github.com>
2024-03-06 18:01:59 +0700

    Update memory.py
    No ApiKey

--- memory.py
+++ memory.py
@@ -8,7 +8,7 @@ import gradio as gr
 import time
 
 # Mengatur API key
-openai_api_key = "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"  # Ganti dengan API key kamu
+openai_api_key = ""  # Ganti dengan API key kamu
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
 # Mengatur memori percakapan
