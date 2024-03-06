498d6c4050a9a2d329bfa7411c72c460b6c7295d
Gufron Aridho Triasa Putra Irawan <119670148+GufronAridho@users.noreply.github.com>
2024-03-06 18:00:36 +0700

    Update coderunner.py
    No ApiKey

--- coderunner.py
+++ coderunner.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_experimental.tools import PythonREPLTool
 from langchain_experimental.utilities import PythonREPL
 
-openai_api_key = "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"
+openai_api_key = ""
 
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
