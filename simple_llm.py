c66953d2dbebe92f217ac56213124e65e72fa6d5
Gufron Aridho Triasa Putra Irawan <119670148+GufronAridho@users.noreply.github.com>
2024-03-06 18:02:57 +0700

    Update simple_llm.py
    No ApiKey

--- simple_llm.py
+++ simple_llm.py
@@ -3,7 +3,7 @@ from langchain_openai import ChatOpenAI
 import gradio as gr
 
 # Memasukkan API key
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = ""
 
 gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )
 
